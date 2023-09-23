44589c37422c4cef7d2945e9998c14544d410f21
ofresia01 <108252053+ofresia01@users.noreply.github.com>
2023-09-22 22:50:05 -0500

    project initialization

---
+++ src/main/java/com/mitsurishi/repairdbapi/RepairDbApiApplication.java
@@ -0,0 +1,11 @@
+package com.mitsurishi.repairdbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairDbApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RepairDbApiApplication.class, args);
+	}
+}
