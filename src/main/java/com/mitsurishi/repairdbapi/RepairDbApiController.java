1c931a22118e30d46333c52799a3a545ad09bdfb
ofresia01 <108252053+ofresia01@users.noreply.github.com>
2023-09-23 01:24:56 -0500

    add mapped loginStatus GET method for login endpoint

--- src/main/java/com/mitsurishi/repairdbapi/RepairDbApiController.java
+++ src/main/java/com/mitsurishi/repairdbapi/RepairDbApiController.java
@@ -1,9 +1,14 @@
 package com.mitsurishi.repairdbapi;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 // Stereotype annotation indicating this is a controller class for handling client requests
 @Controller
 public class RepairDbApiController {
-
+    // Login endpoint, GET method
+    @GetMapping("/login")
+    public boolean getLoginStatus() {
+        return true;
+    }
 }
