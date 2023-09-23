eff32172bfb84b355a3b0c463b7fdd7de0f372da
ofresia01 <108252053+ofresia01@users.noreply.github.com>
2023-09-23 01:13:34 -0500

    add controller class

---
+++ src/main/java/com/mitsurishi/repairdbapi/RepairDbApiController.java
@@ -0,0 +1,9 @@
+package com.mitsurishi.repairdbapi;
+
+import org.springframework.stereotype.Controller;
+
+// Stereotype annotation indicating this is a controller class for handling client requests
+@Controller
+public class RepairDbApiController {
+
+}
