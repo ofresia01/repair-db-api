4b3caeff5adaf01aa4750a387d86b63e315f8a97
ofresia01 <108252053+ofresia01@users.noreply.github.com>
2023-10-01 03:04:06 -0500

    add templated request handling format

--- src/main/java/com/mitsurishi/repairdbapi/RepairDbApiController.java
+++ src/main/java/com/mitsurishi/repairdbapi/RepairDbApiController.java
@@ -2,13 +2,23 @@ package com.mitsurishi.repairdbapi;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.service.annotation.HttpExchange;
 
 // Stereotype annotation indicating this is a controller class for handling client requests
 @Controller
 public class RepairDbApiController {
-    // Login endpoint, GET method
+    /* TEMPLATE FOR ADDING HANDLING TO AN ENDPOINT
+     * @GetMapping("/example") { is a Spring annotation for handling GET requests on the "example" endpoint.
+     * public bool isExample() { is a function that will execute upon GET request of "example" endpoint.
+     *      return true;
+     * }
+     * 
+     * 
+     * 
+     * 
+     */
     @GetMapping("/login")
-    public boolean getLoginStatus() {
+    public Boolean getLogin() {
         return true;
     }
 }
