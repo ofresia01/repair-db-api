df75a617cb67e0b501054c5160329de4d19f4252
Tungsten533 <36699528+Tungsten533@users.noreply.github.com>
2023-11-22 15:09:37 -0600

    Added implementation for 2 classes
    implemented the Employee.java and EmployeeRepository.java

--- src/main/java/com/mitsurishi/repairdbapi/data/models/Employee.java
+++ src/main/java/com/mitsurishi/repairdbapi/data/models/Employee.java
@@ -1,5 +1,71 @@
+/*
+ * Invoice domain class for JPA-based data store, which allows JPA to handle database interactions.
+ */
 package com.mitsurishi.repairdbapi.data.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import java.util.Objects;
+
+@Entity // JPA annotation that prepares object for storage in JPA based data store
 public class Employee {
+    // Attributes (all private)
+    private @Id @GeneratedValue Integer id; // JPA annotations indicating id as auto-generated (via JPA provider) primary key
+    private String name;
+
+    // Default empty constructor
+    Employee() {
+    }
+
+    // Custom constructor for creating this domain object without yet having an ID
+    Employee(String name) {
+        this.name = name;
+    }
+
+    // Getters
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    // Setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
+    // Overwrite .equals(), .hashCode(), and .toString()
+    @Override
+    public boolean equals(Object object) {
+        // Is object the same instance?
+        if (this == object)
+            return true;
+
+        // Is object of the Employee type?
+        if (!(object instanceof Employee))
+            return false;
+        
+        // Cast object to Employee, check if all attributes are equal
+        Employee emp = (Employee) object;
+        return Objects.equals(this.id, emp.id) && Objects.equals(this.name, emp.name);
+    }
+
+    @Override
+    public int hashCode() {
+        // Computes hash value for this object
+        return Objects.hash(this.id, this.name);
+    }
+
+    @Override
+    public String toString() {
+        // return string rep of Employee object
+        return "Employee{" + "id=" + this.id + "name=" + this.name + "}";
+    }
 }
--- src/main/java/com/mitsurishi/repairdbapi/data/repositories/EmployeeRepository.java
+++ src/main/java/com/mitsurishi/repairdbapi/data/repositories/EmployeeRepository.java
@@ -1,5 +1,17 @@
+/*
+ * Spring Data JPA repository interface class for Employee. Supports creating, reading, updating, and deleting records against back-end data store.
+ * Allows for:
+ *   - Creating new Employees
+ *   - Updating existing Employees
+ *   - Deleting Employees
+ *   - Finding Employees (one, all, or by searching with simple to complex parameters)
+ */
 package com.mitsurishi.repairdbapi.data.repositories;
 
-public class EmployeeRepository {
-    
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.mitsurishi.repairdbapi.data.models.Employee;
+
+@Repository // Specify class as Repository bean for component scanning
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {} // Declare interface extension of JpaRepository, specifying domain type as Employee and ID type as Integer
+
