e0748581f05e09661d9bc213cc2f8e50ff235ea3
Conrad00000 <cjvillh@ilstu.edu>
2023-11-25 10:50:58 -0600

    created Ticket Repository and Service class

--- src/main/java/com/mitsurishi/repairdbapi/data/repositories/TicketRepository.java
+++ src/main/java/com/mitsurishi/repairdbapi/data/repositories/TicketRepository.java
@@ -1,5 +1,7 @@
 package com.mitsurishi.repairdbapi.data.repositories;
 
-public class TicketRepository {
-    
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.mitsurishi.repairdbapi.data.models.Ticket;
+@Repository // Specify class as Repository bean for component scanning
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {} 
