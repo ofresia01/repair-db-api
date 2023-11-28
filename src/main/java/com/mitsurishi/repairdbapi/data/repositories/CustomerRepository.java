62beb46594be987ff8a207c2ac7028956d840e2f
Tungsten533 <36699528+Tungsten533@users.noreply.github.com>
2023-11-28 14:36:08 -0600

    Filled in CustomerRepository

--- src/main/java/com/mitsurishi/repairdbapi/data/repositories/CustomerRepository.java
+++ src/main/java/com/mitsurishi/repairdbapi/data/repositories/CustomerRepository.java
@@ -1,5 +1,16 @@
+/*
+ * Spring Data JPA repository interface class for Employee. Supports creating, reading, updating, and deleting records against back-end data store.
+ * Allows for:
+ *   - Creating new Employees
+ *   - Updating existing Employees
+ *   - Deleting Employees
+ *   - Finding Employees (one, all, or by searching with simple to complex parameters)
+ */
 package com.mitsurishi.repairdbapi.data.repositories;
 
-public class CustomerRepository {
-    
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.mitsurishi.repairdbapi.data.models.Customer;
+
+@Repository // Specify class as Repository bean for component scanning
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {} // Declare interface extension of JpaRepository, specifying domain type as Employee and ID type as Integer
