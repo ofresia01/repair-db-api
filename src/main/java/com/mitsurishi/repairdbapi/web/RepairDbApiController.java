092020d3b70a82b5494680340799861d15523154
Conrad00000 <cjvillh@ilstu.edu>
2023-12-06 17:08:05 -0600

    updated controler

--- src/main/java/com/mitsurishi/repairdbapi/web/RepairDbApiController.java
+++ src/main/java/com/mitsurishi/repairdbapi/web/RepairDbApiController.java
@@ -19,9 +19,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mitsurishi.repairdbapi.data.models.Invoice;
+import com.mitsurishi.repairdbapi.data.models.Ticket;
 import com.mitsurishi.repairdbapi.data.models.Customer;
 import com.mitsurishi.repairdbapi.data.payloads.response.MessageResponse;
 import com.mitsurishi.repairdbapi.service.CustomerService;
+import com.mitsurishi.repairdbapi.service.EmployeeService;
 import com.mitsurishi.repairdbapi.service.InvoiceService;
 import com.mitsurishi.repairdbapi.service.TicketService;
 
@@ -38,6 +40,9 @@ public class RepairDbApiController {
 
     @Autowired
     TicketService ticketService;
+
+    @Autowired
+    EmployeeService employeeService;
 
     /*
      * ----------------------INVOICE REQUESTS-----------------------
@@ -68,6 +73,19 @@ public class RepairDbApiController {
         MessageResponse response = customerService.updateCustomer(id,name,email,phoneNumber);
         return new ResponseEntity<MessageResponse>(response, HttpStatus.OK);
     }
+
+    // @PostMapping("/login")
+    // public ResponseEntity<MessageResponse> login(@RequestParam String userName, @RequestParam String password){
+    //     MessageResponse response = employeeService.login(userName,password);
+    //     return new ResponseEntity<MessageResponse>(response, HttpStatus.OK);
+    // }
+
+    @GetMapping("/ticket")
+    public ResponseEntity<List<Ticket>> getTickets(){
+        List<Ticket> tickets = ticketService.getAllTickets();
+        return new ResponseEntity<>(tickets, HttpStatus.OK);
+    }
+    
     // @GetMapping("/invoices/find/{id}")
     // public ResponseEntity<Invoice> getInvoiceById(@PathVariable("id") Integer id) {
     //     Invoice invoice = invoiceService.getSingleInvoice(id);
