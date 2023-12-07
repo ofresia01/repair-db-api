e60588f0014a0988a3edfdf3164e9caf8f29d2d6
Conrad00000 <cjvillh@ilstu.edu>
2023-12-06 21:29:41 -0600

    updated access origin

--- src/main/java/com/mitsurishi/repairdbapi/config/CorsConfig.java
+++ src/main/java/com/mitsurishi/repairdbapi/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/customers/**")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
         // Add more configurations if needed
