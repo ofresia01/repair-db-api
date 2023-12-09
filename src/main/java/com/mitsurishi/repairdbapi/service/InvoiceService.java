522df414713211d1d035a2bb1c8153be2cabd0d1
ofresia01 <108252053+ofresia01@users.noreply.github.com>
2023-12-09 17:40:52 -0600

    complete implementation of InvoiceService

--- src/main/java/com/mitsurishi/repairdbapi/service/InvoiceService.java
+++ src/main/java/com/mitsurishi/repairdbapi/service/InvoiceService.java
@@ -6,9 +6,11 @@ package com.mitsurishi.repairdbapi.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mitsurishi.repairdbapi.data.repositories.InvoiceItemRepository;
 import com.mitsurishi.repairdbapi.data.repositories.InvoiceRepository;
 import com.mitsurishi.repairdbapi.data.repositories.TicketRepository;
 import com.mitsurishi.repairdbapi.data.models.Invoice;
+import com.mitsurishi.repairdbapi.data.models.InvoiceItem;
 import com.mitsurishi.repairdbapi.data.models.Ticket;
 import com.mitsurishi.repairdbapi.data.payloads.response.MessageResponse;
 import com.mitsurishi.repairdbapi.exception.ResourceNotFoundException;
@@ -17,17 +19,23 @@ import java.util.Optional;
 
 @Service // Specify class as Service bean for component scanning
 public class InvoiceService {
-    // Instantiate repository object for JPA data store operations, autowire for dependency injection
+    // Instantiate repository object for JPA data store operations, autowire for
+    // dependency injection
     @Autowired
     InvoiceRepository invoiceRepository;
 
     @Autowired
     TicketRepository ticketRepository;
 
+    @Autowired
+    InvoiceItemRepository invoiceItemRepository;
+
     /**
      * Method that creates an invoice given a ticket.
+     * 
      * @param ticketId ID of the ticket this invoice belongs to.
-     * @return MessageResponse indicating success, otherwise a ResourceNotFoundException.
+     * @return MessageResponse indicating success, otherwise a
+     *         ResourceNotFoundException.
      */
     public MessageResponse createInvoice(Integer ticketId) {
         Optional<Ticket> ticket = ticketRepository.findById(ticketId);
@@ -40,7 +48,31 @@ public class InvoiceService {
         }
     }
 
-    
+    /**
+     * Method that inserts an InvoiceItem (associated with an invoice) in database
+     * via JPA.
+     * 
+     * @param invoiceId Integer identifier of invoice in database
+     * @param name      String containing name of subject of InvoiceItem
+     * @param cost      Integer containing cost of InvoiceItem subject
+     * @param quantity  Integer containing quanitity of InvoiceItem subject
+     * @param type      String indicating type of InvoiceItem
+     * @return MessageResponse indicating success
+     */
+    public MessageResponse updateInvoice(Integer invoiceId, String name, Integer cost, Integer quantity, String type) {
+        // Find the invoice by ID
+        Optional<Invoice> optionalInvoice = invoiceRepository.findById(invoiceId);
 
-    
+        if (optionalInvoice.isPresent()) {
+            // Create a new InvoiceItem
+            InvoiceItem invoiceItem = new InvoiceItem(name, cost, quantity, type, optionalInvoice.get());
+
+            // Save the InvoiceItem
+            invoiceItemRepository.save(invoiceItem);
+
+            return new MessageResponse("InvoiceItem added successfully");
+        } else {
+            throw new ResourceNotFoundException("Invoice", "invoiceId", invoiceId);
+        }
+    }
 }
